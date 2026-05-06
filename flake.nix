54a3e7045f1c38bb9d22b2000ace54709d22bb4b
Valyn Tyler <git@valyntyler.com>
2026-05-06 21:26:47 +0200

    fix: lanzaboote pkgs pin

--- flake.nix
+++ flake.nix
@@ -18,7 +18,6 @@
     musnix.inputs.nixpkgs.follows = "nixpkgs";
 
     lanzaboote.url = "github:nix-community/lanzaboote/v1.0.0";
-    lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
 
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
