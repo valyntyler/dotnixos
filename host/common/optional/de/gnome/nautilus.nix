0560934d2691341c6c621014573d97f6b9e5ec89
Valyn Tyler <git@valyntyler.com>
2026-05-19 18:24:37 +0200

    chore: remove turtle

--- host/common/optional/de/gnome/nautilus.nix
+++ host/common/optional/de/gnome/nautilus.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [turtle];
+  # environment.systemPackages = with pkgs; [turtle];
   programs.nautilus-open-any-terminal = {
     enable = true;
     terminal = "ghostty";
