05be562a0bc86df399f616f84a211e6f82d5a5b5
Valyn Tyler <git@valyntyler.com>
2026-05-26 16:19:15 +0200

    feat: create qui.nix

--- host/desktop/default.nix
+++ host/desktop/default.nix
@@ -26,6 +26,7 @@
     ../common/optional/services/nextcloud.nix
     ../common/optional/services/nginx.nix
     ../common/optional/services/qbittorrent.nix
+    ../common/optional/services/qui.nix
     ../common/optional/services/syncthing.nix
   ];
 
