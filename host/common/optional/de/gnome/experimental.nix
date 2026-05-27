3f7d9180d1085f1791a3672c08f50627fe97a565
Valyn Tyler <git@valyntyler.com>
2026-05-27 11:16:05 +0200

    fix: experimental

--- host/common/optional/de/gnome/experimental.nix
+++ host/common/optional/de/gnome/experimental.nix
@@ -4,6 +4,7 @@
       settings = {
         "org/gnome/mutter" = {
           experimental-features = [
+            "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
             "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
             "autoclose-xwayland" # automatically terminates Xwayland if all relevant X11 clients are gone
           ];
