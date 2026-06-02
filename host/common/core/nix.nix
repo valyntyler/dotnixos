4d080a6a9ce965e45bbb919fe523571d4dacb586
Valyn Tyler <git@valyntyler.com>
2026-06-02 14:13:12 +0200

    chore: extra substituters

--- host/common/core/nix.nix
+++ host/common/core/nix.nix
@@ -6,8 +6,14 @@
     experimental-features = ["nix-command" "flakes"];
 
     # Add binary cache
-    trusted-substituters = ["https://nix-community.cachix.org"];
-    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    trusted-substituters = [
+      "https://nix-community.cachix.org"
+      "https://nix-gaming.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
 
     # Increase download size
     download-buffer-size = 524288000;
