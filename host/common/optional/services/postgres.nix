c8c8e1eccc88fc897cffd173fe1d154421a5514c
Valyn Tyler <git@valyntyler.com>
2026-06-08 23:08:39 +0200

    fix: postgres access

--- host/common/optional/services/postgres.nix
+++ host/common/optional/services/postgres.nix
@@ -4,7 +4,9 @@
     ensureDatabases = ["mydatabase"];
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
-      local all       all     trust
+      #local all       all     trust
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
     '';
   };
 }
