4c0eb9f5586e3206ee04bc3df39a9fdc3c2cb2a5
Valyn Tyler <git@valyntyler.com>
2026-06-09 15:33:31 +0200

    fix: direnv

--- home/common/terminal/programs/direnv.nix
+++ home/common/terminal/programs/direnv.nix
@@ -1,4 +1,9 @@
 {
-  programs.direnv.enable = true;
-  programs.direnv.silent = true;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    silent = true;
+    enableNushellIntegration = true;
+    # config.hide_env_diff = true;
+  };
 }
