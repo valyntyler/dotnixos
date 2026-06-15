4c6118ac2a3eea9022434db8f840bcffd3afdc06
Valyn Tyler <git@valyntyler.com>
2026-06-15 15:13:34 +0200

    chore: update transmission

--- host/common/optional/services/transmission.nix
+++ host/common/optional/services/transmission.nix
@@ -1,10 +1,10 @@
 {
   config,
   pkgs,
+  user,
   ...
 }: let
   cfg = config.services.transmission;
-  # sopsPath = "qui/session";
 in {
   services.transmission = {
     enable = true;
@@ -16,13 +16,16 @@ in {
       rpc-whitelist = "127.0.0.1";
       rpc-whitelist-enabled = false;
       rpc-host-whitelist-enabled = false;
+      rpc-authentication-required = true;
+      rpc-username = "admin";
+      rpc-password = "{da51cc9753f84bfd0b978a34a810d2c0fb725ef63ckwCq6W";
+      # set appropriate permissions
+      download-dir = "/home/${user}/Downloads";
     };
-    # secretFile = config.sops.secrets."${sopsPath}".path;
   };
   services.nginx.virtualHosts."transmission.valyntyler.com" = {
     forceSSL = true;
     enableACME = true;
     locations."/".proxyPass = "http://127.0.0.1:${builtins.toString cfg.settings.rpc-port}";
   };
-  # sops.secrets."${sopsPath}".owner = cfg.user;
 }
