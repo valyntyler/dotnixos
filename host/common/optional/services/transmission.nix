20532c2be5ee60f79860d0b438f388bd860f98f3
Valyn Tyler <git@valyntyler.com>
2026-07-08 02:31:15 +0200

    refactor: transmission secrets

--- host/common/optional/services/transmission.nix
+++ host/common/optional/services/transmission.nix
@@ -17,15 +17,20 @@ in {
       rpc-whitelist-enabled = false;
       rpc-host-whitelist-enabled = false;
       rpc-authentication-required = true;
-      rpc-username = "admin";
-      rpc-password = "{da51cc9753f84bfd0b978a34a810d2c0fb725ef63ckwCq6W";
+      # rpc-username = "admin";
+      # rpc-password = "{da51cc9753f84bfd0b978a34a810d2c0fb725ef63ckwCq6W";
       # set appropriate permissions
       download-dir = "/home/${user}/Downloads";
     };
+    credentialsFile = config.sops.secrets."transmission/json".path;
   };
   services.nginx.virtualHosts."transmission.valyntyler.com" = {
     forceSSL = true;
     enableACME = true;
     locations."/".proxyPass = "http://127.0.0.1:${builtins.toString cfg.settings.rpc-port}";
+  };
+  sops.secrets."transmission/json" = {
+    owner = cfg.user;
+    restartUnits = ["transmission.service"];
   };
 }
