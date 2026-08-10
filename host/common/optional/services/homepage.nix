d971a33ec86573229d59aea21b5f6c0ef6dbcf36
Valyn Tyler <git@valyntyler.com>
2026-08-10 03:33:43 +0300

    feat: update homepage.nix

--- host/common/optional/services/homepage.nix
+++ host/common/optional/services/homepage.nix
@@ -1,3 +1,35 @@
-{
-  # services.homepage-dashboard.enable = true;
+let
+  listenPort = 8082;
+in {
+  services.homepage-dashboard = {
+    inherit listenPort;
+    enable = true;
+    openFirewall = true;
+    allowedHosts = "dash.valyntyler.com";
+    widgets = [
+      {
+        greeting = {
+          text_size = "xl";
+          text = "Hello, world!";
+        };
+      }
+      {
+        datetime = {
+          text_size = "xl";
+          format = {timeStyle = "short";};
+        };
+      }
+      {
+        search = {
+          provider = "duckduckgo";
+          target = "_blank";
+        };
+      }
+    ];
+  };
+  services.nginx.virtualHosts."dash.valyntyler.com" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/".proxyPass = "http://127.0.0.1:${toString listenPort}";
+  };
 }
