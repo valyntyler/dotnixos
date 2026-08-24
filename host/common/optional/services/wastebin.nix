2b424dd9c820837b27c70f31d29f9f5f928386bb
Valyn Tyler <git@valyntyler.com>
2026-08-24 19:54:33 +0300

    feat: update pastebin

--- host/common/optional/services/wastebin.nix
+++ host/common/optional/services/wastebin.nix
@@ -1,5 +1,18 @@
-{
+let
+  port = 8088;
+  domain = "paste.valyntyler.com";
+in {
   services.wastebin = {
     enable = true;
+    settings = {
+      WASTEBIN_ADDRESS_PORT = "0.0.0.0:${toString port}";
+      WASTEBIN_BASE_URL = "https://${domain}";
+      WASTEBIN_TITLE = "Pastebin";
+    };
+  };
+  services.nginx.virtualHosts."${domain}" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/".proxyPass = "http://127.0.0.1:${toString port}";
   };
 }
