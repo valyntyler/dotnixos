ff1c9fec7dfbd1d10fd1c323275033250d58a1ca
Valyn Tyler <git@valyntyler.com>
2026-08-26 13:55:16 +0300

    refactor: move app services

---
+++ host/common/optional/services/app/kanata.nix
@@ -0,0 +1,12 @@
+{
+  services.kanata = {
+    enable = true;
+    keyboards.genericKeyboard = {
+      extraDefCfg = "process-unmapped-keys yes";
+      config = ''
+        (defsrc caps)
+        (deflayer start esc)
+      '';
+    };
+  };
+}
