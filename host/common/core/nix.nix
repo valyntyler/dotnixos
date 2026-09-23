cddf119ed95f6849b2e7d94357b71bfb7174b1b8
Valyn Tyler <git@valyntyler.com>
2026-09-23 20:17:53 +0200

    chore: update nix.nix

--- host/common/core/nix.nix
+++ host/common/core/nix.nix
@@ -1,16 +1,12 @@
 {
   nix.settings = {
     auto-optimise-store = true;
-
-    # Enable flakes and nix command
     experimental-features = [
       "flakes"
       "nix-command"
       "pipe-operators"
     ];
-
-    # Add binary cache
-    trusted-substituters = [
+    substituters = [
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
     ];
@@ -18,8 +14,6 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
-
-    # Increase download size
     download-buffer-size = 524288000;
   };
 }
